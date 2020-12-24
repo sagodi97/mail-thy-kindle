69c75e6ca2ce5f18a7e943eb936df7d6397ced26
Santiago Gonzalez <santiago.gonzalez@clarite.pl>
2020-12-24 01:26:21 +0100

    Add nodemailer and setup SMTP transport

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const nodemailer = require("nodemailer");
 
 const port = 4000 | process.env.PORT;
 
 app.use(cors());
+
+let transport = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSW,
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("OK");
