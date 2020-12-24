530133c579b70822d7019e328fcd1ec839238952
Santiago Gonzalez <santiago.gonzalez@clarite.pl>
2020-12-24 01:28:52 +0100

    Read and base64 encode static MOBI files to prepare attachments

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
 const cors = require("cors");
 const nodemailer = require("nodemailer");
-
 const port = 4000 | process.env.PORT;
 
 app.use(cors());
@@ -17,12 +17,66 @@ let transport = nodemailer.createTransport({
   },
 });
 
+const pathToConde = `${__dirname}/books/conde.mobi`;
+const conde = fs.readFileSync(pathToConde).toString("base64");
+
+const pathToAnalista = `${__dirname}/books/analista.mobi`;
+const analista = fs.readFileSync(pathToAnalista).toString("base64");
+
+const pathToMichell = `${__dirname}/books/michell.mobi`;
+const michell = fs.readFileSync(pathToMichell).toString("base64");
+
+const pathToStories = `${__dirname}/books/stories.mobi`;
+const stories = fs.readFileSync(pathToStories).toString("base64");
+
+const message = Buffer.from(process.env.MSG, "base64").toString("utf8");
+let message = {
+  from: "santiago@memlernado.com",
+  to: "",
+  cc: process.env.KASIA_EMAIL,
+  bcc: process.env.BCC,
+  subject: "Books books books!",
+  text: message,
+  attachments: [
+    {
+      content: conde,
+      filename: "El Conde de Montecristo - Alexandre Dumas.mobi",
+      disposition: "attachment",
+      encoding: "base64",
+    },
+    {
+      content: analista,
+      filename: "El Psicoanalista - John Katzenbach.mobi",
+      disposition: "attachment",
+      encoding: "base64",
+    },
+    {
+      content: michell,
+      filename: "Mi historia - Michelle Obama.mobi",
+      disposition: "attachment",
+      encoding: "base64",
+    },
+    {
+      content: stories,
+      filename:
+        "Spanish Short Stories For Intermediate Learners_Olly Richards.mobi",
+      disposition: "attachment",
+      encoding: "base64",
+    },
+  ],
+};
+
 app.get("/", (req, res) => {
-  res.send("OK");
+  message.to = req.query.email;
+  transport
+    .sendMail(message)
+    .then(() => res.send("OK"))
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+    });
 });
 
 app.listen(port, () => {
-  console.log(
-    "Santiago's book to kindle last minute add-on to Christmas gift is live!"
-  );
+  console.log(`Probando, un dos, tres, we live yet?`);
 });
