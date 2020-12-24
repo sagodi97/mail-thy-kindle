02b77b79db89f841d4b64bd79307c8ff91ae7e75
Santiago Gonzalez <santiago.gonzalez@clarite.pl>
2020-12-24 01:24:06 +0100

    Add CORS support

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const port = 4000 | process.env.PORT;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("OK");
